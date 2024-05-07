14ecbb727cf02dc8f8dbf332e5db44d6ee97fa3c
1bertojunior <hjunior854@gmail.com>
2024-05-07 19:48:43 -0300

    feat: Add models for users, cities, states, and addresses

---
+++ app/Models/Address.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'street_address', 'neighborhood', 'city_id', 'number', 'postal_code', 'complement'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function state()
+    {
+        return $this->belongsTo(State::class);
+    }
+}
